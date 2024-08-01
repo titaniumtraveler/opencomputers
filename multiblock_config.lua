77ce7d55e2268313cc3d22adebd598430d425f6e
Jon Heinritz <jon.heinritz@protonmail.com>
2024-08-01 16:00:03 +0200

    feat(config): add default config

---
+++ multiblock_config.lua
@@ -0,0 +1,12 @@
+---@module "multiblock.types.config"
+---@type Multiblock.Config
+return {
+  center = { -4, -1, -2 },
+  drop = { 0, 4, 0 },
+  recipes = {
+    "multiblock.recipes.ender_pearl",
+    "multiblock.recipes.compact_normal",
+    "multiblock.recipes.compact_wall",
+    "multiblock.recipes.compact_giant_sm",
+  },
+}
